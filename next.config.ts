7b509472a2b56bb109c4cfc2fea95c7e4249abf6
sureshamal <sureshambechada@proton.me>
2026-04-20 00:40:05 +0530

    added nextjs dependency

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     serverActions: {
-      bodySizeLimit: '10mb', // Can be in bytes or string format (e.g., '500kb', '10mb')
+      bodySizeLimit: '10mb',
     },
+
   },
+  serverExternalPackages: ["isomorphic-dompurify"],
   images: {
     remotePatterns: [
       {
